9f9f472014ac750768a6a5aaa3c1b7856c5c16c3
Zahed Miah <zahedm70@gmail.com>
2023-03-31 11:35:54 +0100

    testing

--- src/components/TestingPage.svelte
+++ src/components/TestingPage.svelte
@@ -1,9 +1,12 @@
 <script>
   import SubEvent from "./SubEvent.svelte";
   import Gallery from "./Gallery.svelte";
+  import OpenStreetMap from "./OpenStreetMap.svelte";
 </script>
 
 <main>
   <h1>Page for testing components!</h1>
   <SubEvent />
+  <Gallery />
+  <OpenStreetMap />
 </main>
