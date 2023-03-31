54418b5038b24aeeb3d4295e0fe9e381adc53096
Zahed Miah <zahedm70@gmail.com>
2023-03-31 11:00:02 +0100

    gallery

--- src/components/TestingPage.svelte
+++ src/components/TestingPage.svelte
@@ -1,10 +1,10 @@
 <script>
   import SubEvent from "./SubEvent.svelte";
-
-
+  import Gallery from "./Gallery.svelte";
 </script>
 
 <main>
   <h1>Page for testing components!</h1>
   <SubEvent />
+  <Gallery />
 </main>
