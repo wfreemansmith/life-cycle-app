221749dd0350d252cecd7a929c948743c70151f9
Wes <wfreemansmith@yahoo.co.uk>
2023-04-03 11:56:56 +0100

    menu toggle sorted

--- src/components/Tree.svelte
+++ src/components/Tree.svelte
@@ -13,24 +13,31 @@
 
   // Function to create a new milestone at any point on the tree
   const addMilestone = (name) => {
-    tree.forEach((milestone) => {
-      milestone.menu = null;
-      if (!milestone.name & !milestone.detail) deleteLifeEvent(milestone.name);
-    });
+    closeAllPopOuts()
     const position = tree.findIndex((milestone) => {
       return milestone.name === name;
     });
 
-    const newEvent = {
+    const newMilestone = {
       id: tree.length + 1,
       name: "",
       detail: "",
       date: "",
       menu: "form",
     };
-    tree.splice(position + 1, 0, newEvent);
+    tree.splice(position + 1, 0, newMilestone);
     tree = tree;
   };
+
+  // closes all sub event menus
+  const closeAllPopOuts = () => {
+    console.log("hello")
+    tree.forEach((milestone) => {
+      milestone.menu = null;
+      if (!milestone.name & !milestone.detail) deleteLifeEvent(milestone.name);
+    });
+    tree = tree;
+  }
 
   // Orders tree array of Milestones by date
   const orderByDate = () => {
@@ -86,6 +93,8 @@
     tree = [...tree, ...Object.values(user.milestones)];
     orderByDate();
   }
+
+  closeAllPopOuts()
 </script>
 
 <main class="flex bg-black flex-col items-center w-[99vw] h-full relative">
@@ -136,7 +145,7 @@
     </svg>
   </div>
   {#each tree as milestone}
-    <Poi {milestone} {addMilestone} {deleteLifeEvent} {user} {orderByDate} />
+    <Poi {milestone} {addMilestone} {deleteLifeEvent} {user} {orderByDate} {closeAllPopOuts}/>
   {/each}
 </main>
 
