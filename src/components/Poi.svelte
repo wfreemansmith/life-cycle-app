d3dd941807ebe2df950f1e334d496581ed859df4
munist1010 <aaronjkellywork@gmail.com>
2023-04-04 06:08:26 -0700

    changes to button styling on POI

--- src/components/Poi.svelte
+++ src/components/Poi.svelte
@@ -149,6 +149,16 @@
   }
   .card-wrapper {
     transition: width 4s;
+    height: 200px;
+  }
+
+  .new-main {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+    flex-direction: column;
+    align-items: center;
+    height: 200px;
   }
 
   form {
@@ -205,11 +215,10 @@ main:before {
 
   button {
     background-color: #ed203d;
-    margin: auto;
     min-width: 15px;
-    min-height: 15px;
     border: none;
     padding: 2px;
+    margin-bottom: 20px
   }
 
   .add-event {
